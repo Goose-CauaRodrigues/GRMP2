662b906d6ed18a99b2b4d07d0ba7ffd507c6c630
Cauã Rodrigues <caua.rodigues1211@gmail.com>
2026-05-14 19:31:37 -0300

    Construção do Login

--- GRMP/Program.cs
+++ GRMP/Program.cs
@@ -3,6 +3,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddAuthentication("Cookies")
+    .AddCookie("Cookies", options =>
+    {
+        // Caminho para a página de login
+        options.LoginPath = "/Login/Login";
+
+        // Caminho para logout
+        options.LogoutPath = "/Login/Logout";
+
+        // Opcional: define onde o usuário será redirecionado após login
+        options.AccessDeniedPath = "/Login/Login";
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
