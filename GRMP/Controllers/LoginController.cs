d1895691e8a209dc2bdd9f8cbdbac65bdaf4427f
Treinamentos TI - CFP 6.02 <treinamentosti@educ602.local>
2026-05-13 22:31:44 -0300

    Construção do Login

--- GRMP/Controllers/LoginController.cs
+++ GRMP/Controllers/LoginController.cs
@@ -1,4 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using GRMP.Classes;
+using GRMP.Models;
+using System.Data;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
 
 namespace GRMP.Controllers
 {
@@ -7,6 +15,19 @@ namespace GRMP.Controllers
         public IActionResult Login()
         {
             return View("LoginView");
+        }
+
+        public IActionResult LoginProcessar(LoginViewModel Vm_Login)
+        {
+
+            
+            return View();
+        }
+
+        private string GerarHash(string senha)
+        {
+            var bytes = SHA256.HashData(Encoding.UTF8.GetBytes(senha));
+            return Convert.ToHexString(bytes).ToLower();
         }
     }
 }
---
+++ GRMP/Models/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GRMP.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Senha {  get; set; }
+        [Required]
+        public string Email {  get; set; }
+    }
+}
