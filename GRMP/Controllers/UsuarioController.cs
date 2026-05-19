6578b7f06a6b21bc03cde302b9c2a83ffdc5d8fc
Treinamentos TI - CFP 6.02 <treinamentosti@educ602.local>
2026-05-19 19:06:22 -0300

    teste23

--- GRMP/Controllers/UsuarioController.cs
+++ GRMP/Controllers/UsuarioController.cs
@@ -2,12 +2,7 @@
 using GRMP.Classes;
 using GRMP.Models;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Mvc;
-using ProjBancoDados.BancoDados;
 using System.Data;
-using System.Security.Claims;
-using System.Security.Cryptography;
 using System.Text;
 
 namespace GRMP.Controllers
@@ -28,5 +23,6 @@ namespace GRMP.Controllers
 
             return View("InicioView" , dt);
         }
+
     }
 }
