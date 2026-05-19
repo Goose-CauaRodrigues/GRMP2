89306ffb4172e1d4848b845ded228eb7623ae649
Cauã Rodrigues <caua.rodigues1211@gmail.com>
2026-05-18 22:18:31 -0300

    criação da tela de Orden de serviço

---
+++ GRMP/Models/OsViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GRMP.Models
+{
+    public class OrdemServicoViewModel
+    {
+        public int IdOrdemServico { get; set; }
+
+        [Required]
+        public int FkIdUsuario { get; set; }
+
+        public int? FkExecutor { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string DescricaoServico { get; set; }
+
+        [Required]
+        public int Categoria { get; set; }
+
+        public string? NumeroPatrimonio { get; set; }
+
+        [Required]
+        public string Bloco { get; set; }
+
+        [Required]
+        public string Local { get; set; }
+
+        public int? Prioridade { get; set; }
+
+        public string? Observacoes { get; set; }
+
+        [Required]
+        public DateTime DataSolicitacao { get; set; }
+
+        public DateTime? DataInicio { get; set; }
+
+        public DateTime? DataFinalizacao { get; set; }
+
+        public int? Status { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
